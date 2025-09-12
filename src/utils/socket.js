96e5d68deae55167caa742b2eac61021e28111e6
Mairajuddin <qazimairaj4@gmail.com>
2025-09-12 20:28:18 +0500

    update

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,10 @@
 import { io } from "socket.io-client";
 
 
-const SOCKET_URL = "http://192.168.18.33:5000"; 
+// const SOCKET_URL = "http://192.168.18.33:5000"; 
+const SOCKET_URL = "https://labubu2.4xbrokers.com"; 
+
+//https://labubu2.4xbrokers.com
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"], 
